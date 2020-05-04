e7ad531962f8fa5a718913ac6543f92847acd5b1
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2020-05-04 18:06:13 +0200

    Update config

--- portal/config.py
+++ portal/config.py
@@ -12,7 +12,6 @@ class Config:
     OAUTH2_CLIENT_ID = os.getenv('OAUTH2_CLIENT_ID')
     OAUTH2_CLIENT_SECRET = os.getenv('OAUTH2_CLIENT_SECRET')
     OAUTH2_SCOPES = os.getenv('OAUTH2_SCOPES', '').split()
-    OAUTH2_AUDIENCE = os.getenv('OAUTH2_AUDIENCE')
 
     ODP_API_URL = os.getenv('ODP_API_URL')
     ACCOUNTS_API_URL = os.getenv('ACCOUNTS_API_URL')
